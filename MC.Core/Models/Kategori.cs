7bf54df245bb0459d6c221bc10d6b982cd92cee0
Mustafa Çağrı ALTINDAL <mcaltindal@cagrialtindal.com.tr>
2020-11-29 13:43:07 +0300

    Add project files.

---
+++ MC.Core/Models/Kategori.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MC.Core.Models
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string Tanim { get; set; }
+        public bool IsDelected { get; set; }
+    }
+}
---
+++ MC.Core/Models/Urun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MC.Core.Models
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string Tanim { get; set; }
+        public int KategoriId { get; set; }
+        public int Stok { get; set; }
+        public bool Durum { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+        public virtual Kategori Kategori { get; set; }
+    }
+}
